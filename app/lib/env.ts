694affec6ba499746f3705d2dc4942832e8f8eef
hugeblank <hugeblank@gmail.com>
2025-05-06 00:25:01 -0700

    fix: title again

--- app/lib/env.ts
+++ app/lib/env.ts
@@ -2,6 +2,6 @@ import { z } from "zod";
 export const env = z
   .object({
     HANDLE: z.string(),
-    TITLE: z.string().min(1).default("Home"),
+    TITLE: z.string().min(1).optional().default("Home"),
   })
   .parse(process.env);
